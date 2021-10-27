ff4c2c3fcbfcbfe4d3e989374cfddcc49cdffffa
yhh1056 <yhh1056@naver.com>
2021-10-27 18:54:29 +0900

    Fix: Dto 필드 수정

--- src/main/java/com/backmin/domains/menu/dto/MenuCategoryDto.java
+++ src/main/java/com/backmin/domains/menu/dto/MenuCategoryDto.java
@@ -1,9 +1,5 @@
 package com.backmin.domains.menu.dto;
 
-import com.backmin.domains.menu.domain.Menu;
-import com.backmin.domains.store.domain.Store;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +10,5 @@ public class MenuCategoryDto {
     private Long id;
 
     private String name;
-
-    private List<Menu> menus = new ArrayList<>();
-
-    private Store store;
 
 }
--- src/main/java/com/backmin/domains/review/dto/ReviewDto.java
+++ src/main/java/com/backmin/domains/review/dto/ReviewDto.java
@@ -1,7 +1,5 @@
 package com.backmin.domains.review.dto;
 
-import com.backmin.domains.member.domain.Member;
-import com.backmin.domains.store.domain.Store;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +9,7 @@ public class ReviewDto {
 
     private Long id;
 
-    private Store store;
-
     private int score;
-
-    private Member member;
 
     private Long orderId;
 
