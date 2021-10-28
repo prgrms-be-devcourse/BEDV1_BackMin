f14d482483aba6f43cc52475feea38fd83875dce
devrunner21 <devrunner21@gmail.com>
2021-10-28 13:16:43 +0900

    [Feat] 공통 API Response 객체를 정의하라.
    - 공통 API Response 객체 생성
    - 공통 에러 객체 정의
    - ErrorHandler 정의
    - Error 정보를 관리할 Enum 정의

---
+++ src/main/java/com/backmin/domains/common/dto/ApiError.java
@@ -0,0 +1,14 @@
+package com.backmin.domains.common.dto;
+
+public class ApiError {
+
+    private String code;
+
+    private Object message;
+
+    public ApiError(String code, Object message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
