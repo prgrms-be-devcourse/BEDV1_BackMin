19ccf2323f0bb33ae7e2ec8c4897f8a6db89cdbc
devrunner21 <devrunner21@gmail.com>
2021-11-01 16:36:00 +0900

    [Feat] 리뷰 총 개수, 평균점수를 구하는 로직을 구현하고 테스트 코드를 작성하라.

---
+++ src/test/java/com/backmin/domains/review/domain/ReviewRepositoryTest.java
@@ -0,0 +1,208 @@
+package com.backmin.domains.review.domain;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import com.backmin.domains.member.domain.Member;
+import com.backmin.domains.member.domain.MemberRepository;
+import com.backmin.domains.order.domain.Order;
+import com.backmin.domains.order.domain.OrderRepository;
+import com.backmin.domains.order.domain.Payment;
+import com.backmin.domains.store.domain.Category;
+import com.backmin.domains.store.domain.CategoryRepository;
+import com.backmin.domains.store.domain.Store;
+import com.backmin.domains.store.domain.StoreRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@Transactional
+@DisplayName("ReviewRepository 테스트")
+class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    @DisplayName("가게별 총 리뷰 개수 조회 테스트")
+    void getReviewTotalCountByStore() {
+
+        // given
+        Category category1 = Category.builder()
+                .name("한식")
+                .build();
+        Category savedCategory = categoryRepository.save(category1);
+
+        Store store1 = Store.of(
+                "동대문 엽기 떡볶이",
+                "070364532746",
+                "엽떡집입니다.",
+                1000,
+                30,
+                60,
+                2000,
+                true,
+                true,
+                savedCategory,
+                new ArrayList<>() // menus
+        );
+        Store savedStore = storeRepository.save(store1);
+
+        Member member = Member.builder()
+                .email("devrunner21@gmail.com")
+                .address("수원시")
+                .nickName("K")
+                .password("1234")
+                .phoneNumber("01023564756")
+                .build();
+        memberRepository.save(member);
+
+        Order order1 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        Order order2 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        Order order3 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        List<Order> orders = List.of(order1, order2, order3);
+        orderRepository.saveAll(orders);
+
+        Review review1 = Review.of(
+                "맛있습니다.",
+                1,
+                savedStore,
+                member,
+                order1
+        );
+
+        Review review2 = Review.of(
+                "맛있습니다.22",
+                2,
+                savedStore,
+                member,
+                order2
+        );
+        Review review3 = Review.of(
+                "맛있습니다.33",
+                3,
+                savedStore,
+                member,
+                order3
+        );
+
+        List<Review> reviews = List.of(review1, review2, review3);
+        reviewRepository.saveAll(reviews);
+
+        // when
+        int reviewTotalCountByStore = reviewRepository.getReviewTotalCountByStore(savedStore.getId());
+
+        // then
+        assertThat(reviewTotalCountByStore, is(reviews.size()));
+    }
+
+    @Test
+    @DisplayName("가게별 평균 평점 조회 테스트")
+    void getReviewStoreAverageByStore() {
+
+        // given
+        Category category1 = Category.builder()
+                .name("한식")
+                .build();
+        Category savedCategory = categoryRepository.save(category1);
+
+        Store store1 = Store.of(
+                "동대문 엽기 떡볶이",
+                "070364532746",
+                "엽떡집입니다.",
+                1000,
+                30,
+                60,
+                2000,
+                true,
+                true,
+                savedCategory,
+                new ArrayList<>() // menus
+        );
+        Store savedStore = storeRepository.save(store1);
+
+        Member member = Member.builder()
+                .email("devrunner21@gmail.com")
+                .address("수원시")
+                .nickName("K")
+                .password("1234")
+                .phoneNumber("01023564756")
+                .build();
+        memberRepository.save(member);
+
+        Order order1 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        Order order2 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        Order order3 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        Order order4 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        Order order5 = Order.of(member.getAddress(), "양 많이요,", Payment.KAKAO_PAY, member, 2000);
+        List<Order> orders = List.of(order1, order2, order3, order4, order5);
+        orderRepository.saveAll(orders);
+
+        Review review1 = Review.of(
+                "맛있습니다.",
+                1,
+                savedStore,
+                member,
+                order1
+        );
+
+        Review review2 = Review.of(
+                "맛있습니다.22",
+                2,
+                savedStore,
+                member,
+                order2
+        );
+        Review review3 = Review.of(
+                "맛있습니다.33",
+                3,
+                savedStore,
+                member,
+                order3
+        );
+        Review review4 = Review.of(
+                "맛있습니다.44",
+                4,
+                savedStore,
+                member,
+                order4
+        );
+        Review review5 = Review.of(
+                "맛있습니다.55",
+                5,
+                savedStore,
+                member,
+                order5
+        );
+        List<Review> reviews = List.of(review1, review2, review3, review4, review5);
+        reviewRepository.saveAll(reviews);
+
+        // when
+        double reviewStoreAverageByStore = reviewRepository.getReviewAverageByStore(savedStore.getId());
+
+        // then
+        double calculatedAverage = reviews.stream()
+                .mapToDouble(Review::getScore)
+                .average()
+                .orElse(0);
+        assertThat(reviewStoreAverageByStore, is(calculatedAverage));
+    }
+
+}
