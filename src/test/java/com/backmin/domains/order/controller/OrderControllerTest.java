efcfb11aebbfc12c4261c3e254e2b2a6074de252
yhh1056 <yhh1056@naver.com>
2021-11-03 14:09:58 +0900

    Feat: api 문서화

--- src/test/java/com/backmin/domains/order/controller/OrderControllerTest.java
+++ src/test/java/com/backmin/domains/order/controller/OrderControllerTest.java
@@ -1,6 +1,13 @@
 package com.backmin.domains.order.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -34,14 +41,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 class OrderControllerTest {
 
     @Autowired
@@ -102,10 +112,27 @@ class OrderControllerTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(objectMapper.writeValueAsString(createOrderParam)))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("success").isBoolean())
-                .andExpect(jsonPath("data").doesNotExist())
-                .andExpect(jsonPath("serverDatetime").isString());
+                .andDo(document("order-save",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 Id"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
+                                fieldWithPath("requirement").type(JsonFieldType.STRING).description("요구 사항"),
+                                fieldWithPath("storeId").type(JsonFieldType.NUMBER).description("가게 id"),
+                                fieldWithPath("address").type(JsonFieldType.STRING).description("주소"),
+                                fieldWithPath("menuReadParams").type(JsonFieldType.ARRAY).description("메뉴"),
+                                fieldWithPath("menuReadParams[].id").type(JsonFieldType.NUMBER).description("메뉴 Id"),
+                                fieldWithPath("menuReadParams[].quantity").type(JsonFieldType.NUMBER).description("메뉴 주문 수량"),
+                                fieldWithPath("menuReadParams[].menuOptionIds").type(JsonFieldType.ARRAY).description("메뉴 주문 옵션"),
+                                fieldWithPath("payment").type(JsonFieldType.STRING).description("지불 방법")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("상태코드"),
+                                fieldWithPath("data").type(JsonFieldType.NULL).description("응답 데이터"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )
+                ));
 
         Order order = orderRepository.findAll().get(0);   // 떡복이2개 각각 당면추가
         assertThat(order.getAddress()).isEqualTo(createOrderParam.getAddress());
@@ -131,9 +158,21 @@ class OrderControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("success").isBoolean())
-                .andExpect(jsonPath("data").doesNotExist())
-                .andExpect(jsonPath("serverDatetime").isString());
+                .andDo(document("order-status-update",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 Id"),
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
+                                fieldWithPath("orderStatus").type(JsonFieldType.STRING).description("가게 상태")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("상태코드"),
+                                fieldWithPath("data").type(JsonFieldType.NULL).description("응답 데이터"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )
+                ));
 
         Order updatedOrder = orderRepository.findById(savedOrder.getId()).get();
         assertThat(updatedOrder.getStatus()).isEqualTo(OrderStatus.CANCELED);
@@ -199,10 +238,29 @@ class OrderControllerTest {
                 .param("size", String.valueOf(10)))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("data.totalCount").exists())
-                .andExpect(jsonPath("data.pageNumber").exists())
-                .andExpect(jsonPath("data.pageSize").exists())
-                .andExpect(jsonPath("data.list").exists());
+                .andDo(document("orders-page",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("page").description("page"),
+                                parameterWithName("size").description("size")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("상태코드"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
+                                fieldWithPath("data.totalCount").type(JsonFieldType.NUMBER).description("응답 데이터"),
+                                fieldWithPath("data.pageNumber").type(JsonFieldType.NUMBER).description("응답 데이터"),
+                                fieldWithPath("data.pageSize").type(JsonFieldType.NUMBER).description("응답 데이터"),
+                                fieldWithPath("data.list").type(JsonFieldType.ARRAY).description("응답 데이터"),
+                                fieldWithPath("data.list[].orderId").type(JsonFieldType.NUMBER).description("응답 데이터"),
+                                fieldWithPath("data.list[].storeId").type(JsonFieldType.NUMBER).description("응답 데이터"),
+                                fieldWithPath("data.list[].storeName").type(JsonFieldType.STRING).description("응답 데이터"),
+                                fieldWithPath("data.list[].menuReadRespons").type(JsonFieldType.ARRAY).description("응답 데이터"),
+                                fieldWithPath("data.list[].orderDateTime").type(JsonFieldType.STRING).description("응답 데이터"),
+                                fieldWithPath("data.hasNext").type(JsonFieldType.BOOLEAN).description("응답 데이터"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )
+                ));
     }
 
     private Member givenSaveMember() {
