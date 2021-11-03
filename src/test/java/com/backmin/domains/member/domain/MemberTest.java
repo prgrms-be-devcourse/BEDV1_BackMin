11c461ddca4c12cfebfd1fa19e770f6ad79c5217
devrunner21 <devrunner21@gmail.com>
2021-11-03 23:07:48 +0900

    [refactor] 테스트 코드에 DataJpaTest를 SpringBootTest로 변경하라

--- src/test/java/com/backmin/domains/member/domain/MemberTest.java
+++ src/test/java/com/backmin/domains/member/domain/MemberTest.java
@@ -1,16 +1,14 @@
 package com.backmin.domains.member.domain;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
 import java.util.Optional;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DataJpaTest
+@SpringBootTest
 class MemberTest {
 
     @Autowired
