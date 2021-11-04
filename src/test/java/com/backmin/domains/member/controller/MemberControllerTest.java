648fd4b723b9d7b3f730f1dabf471b0e6e5f3d89
Young-BLUE <youngblue.coding@gmail.com>
2021-11-04 16:07:36 +0900

    Feat : MemberControllerTest 테스트 케이스 추가 및 givenMember추가, ErrorCode 추가로 멤버 생성시 이메일과 전화번호 형식 확인

--- src/test/java/com/backmin/domains/member/controller/MemberControllerTest.java
+++ src/test/java/com/backmin/domains/member/controller/MemberControllerTest.java
@@ -9,13 +9,14 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.response
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.backmin.domains.BaseControllerTest;
 import com.backmin.domains.member.domain.Member;
 import com.backmin.domains.member.dto.request.MemberCreateParam;
 import com.backmin.domains.member.dto.request.MemberUpdateParam;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -27,19 +28,8 @@ class MemberControllerTest extends BaseControllerTest {
         memberRepository.deleteAll();
     }
 
-    @BeforeAll
-    void setUp() {
-        Member member = Member.of("testemail01@gmail.com",
-                "testpassword",
-                "010-1112-2222",
-                "야이야이야",
-                "인천광역시"
-        );
-
-        memberRepository.save(member);
-    }
-
     @Test
+    @DisplayName("멤버 생성 API 테스트")
     void create_member() throws Exception {
         MemberCreateParam memberCreateParam = new MemberCreateParam();
         memberCreateParam.setEmail("test@gmail.com");
@@ -68,17 +58,16 @@ class MemberControllerTest extends BaseControllerTest {
     }
 
     @Test
-    void update_member() throws Exception{
-        Member foundMember = memberRepository.findAll().get(0);
-
-        System.out.println(foundMember.getId());
+    @DisplayName("멤버 수정 API 테스트")
+    void update_member() throws Exception {
+        Member givenMember = givenMember();
 
         MemberUpdateParam memberUpdateParam = new MemberUpdateParam();
         memberUpdateParam.setAddress("광주광역시");
         memberUpdateParam.setPassword("testpassword");
         memberUpdateParam.setEmail("testemail01@gmail.com");
 
-        mockMvc.perform(put("/api/v1/bm/members/{id}", foundMember.getId())
+        mockMvc.perform(put("/api/v1/bm/members/{id}", givenMember.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(memberUpdateParam)))
                 .andDo(print())
@@ -99,5 +88,81 @@ class MemberControllerTest extends BaseControllerTest {
 
         Member afterMember = memberRepository.findAll().get(0);
         assertThat(afterMember.getAddress(), is("광주광역시"));
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 이메일로 가입시도 시 오류코드 반환")
+    void invalidEmailAddressTest() throws Exception {
+        MemberCreateParam memberCreateParam = new MemberCreateParam();
+        memberCreateParam.setEmail("test-email-address.;@gmail.com");
+        memberCreateParam.setPassword("test1234");
+        memberCreateParam.setNickName("이구역개발퀸");
+        memberCreateParam.setAddress("부산광역시");
+        memberCreateParam.setPhoneNumber("010-1122-3344");
+
+        mockMvc.perform(post("/api/v1/bm/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateParam)))
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andDo(document("member-invalid-email-save",
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber"),
+                                fieldWithPath("nickName").type(JsonFieldType.STRING).description("nickName"),
+                                fieldWithPath("address").type(JsonFieldType.STRING).description("address")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("데이터"),
+                                fieldWithPath("data.code").type(JsonFieldType.STRING).description("오류 코드"),
+                                fieldWithPath("data.message").type(JsonFieldType.STRING).description("오류 메세지"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )));
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 전화번호로 가입 시 오류코드 반환")
+    void invalidPhoneNumberCheck() throws Exception {
+        MemberCreateParam memberCreateParam = new MemberCreateParam();
+        memberCreateParam.setEmail("testemail00@gmail.com");
+        memberCreateParam.setPassword("test1234");
+        memberCreateParam.setNickName("이구역개발퀸");
+        memberCreateParam.setAddress("부산광역시");
+        memberCreateParam.setPhoneNumber("890-11-344");
+
+        mockMvc.perform(post("/api/v1/bm/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberCreateParam)))
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andDo(document("member-invalid-phoneNumber-save",
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("email"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("password"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("phoneNumber"),
+                                fieldWithPath("nickName").type(JsonFieldType.STRING).description("nickName"),
+                                fieldWithPath("address").type(JsonFieldType.STRING).description("address")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("데이터"),
+                                fieldWithPath("data.code").type(JsonFieldType.STRING).description("오류 코드"),
+                                fieldWithPath("data.message").type(JsonFieldType.STRING).description("오류 메세지"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )));
+    }
+
+    public Member givenMember() {
+        Member member = Member.of("testemail01@gmail.com",
+                "testpassword",
+                "010-1112-2222",
+                "야이야이야",
+                "인천광역시"
+        );
+        memberRepository.save(member);
+
+        return member;
     }
 }
