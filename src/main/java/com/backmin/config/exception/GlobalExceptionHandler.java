cb85838892e1e358380a6d83cff1638ac3fe35ee
yhh1056 <yhh1056@naver.com>
2021-11-04 22:27:03 +0900

    Refactor: config 패키지 라인 수정 및 람다식 변경
    단순 foreach로 map을 채우지 않고 collect toMap을 사용

--- src/main/java/com/backmin/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/backmin/config/exception/GlobalExceptionHandler.java
@@ -3,16 +3,16 @@ package com.backmin.config.exception;
 import com.backmin.domains.common.dto.ApiResult;
 import com.backmin.domains.common.enums.ErrorInfo;
 import com.backmin.config.exception.BusinessException;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,22 +21,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResult<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        log.error("{}", ex);
-
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors()
-            .forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
-
-        return ApiResult.error("METHOD_ARG_NOT_VALID", errors);
+        log.error("{}", ex); /** todo: 에러 로그를 얼마나 찍어야하는지? */
+        return ApiResult.error("METHOD_ARG_NOT_VALID", ex.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(error -> ((FieldError) error).getField(), ObjectError::getDefaultMessage)));
     }
 
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResult<?> handleBusinessExceptionException(BusinessException ex) {
         log.error("{}", ex);
-
         ErrorInfo errorInfo = ex.getErrorInfo();
-
         return ApiResult.error(errorInfo.getCode(), errorInfo.getMessage());
     }
 
@@ -44,9 +38,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResult handleException(Exception ex) {
         log.error("{}", ex);
-
         ErrorInfo errorInfo = ErrorInfo.UNKNOWN;
-
         return ApiResult.error(errorInfo.getCode(), errorInfo.getMessage());
     }
 
